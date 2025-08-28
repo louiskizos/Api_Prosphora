e6fb66dde425b31f7f06bb8392144c20e16ab590
louiskizos <louisnyembo2@gmail.com>
2025-08-28 16:17:26 +0200

    commite okk

--- Api_prosphora_Core/settings.py
+++ Api_prosphora_Core/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -145,7 +145,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # là où les fichiers seront collectés
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),  # optionnel, pour tes fichiers perso
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
