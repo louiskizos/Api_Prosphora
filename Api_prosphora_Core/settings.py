bbe666869c7f1a8f7d8698ccafd1fd04dd7241e4
louiskizos <louisnyembo2@gmail.com>
2026-01-06 13:56:07 +0200

    quarante pourcents %%

--- Api_prosphora_Core/settings.py
+++ Api_prosphora_Core/settings.py
@@ -99,26 +99,26 @@ load_dotenv()
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-# DATABASE_URL = os.getenv("DATABASE_URL")
+DATABASE_URL = os.getenv("DATABASE_URL")
 
-# if DATABASE_URL:
-#     DATABASES = {
-#         'default': dj_database_url.parse(
-#             DATABASE_URL,
-#             conn_max_age=600,
-#             ssl_require=True
-#         )
-#   }
+if DATABASE_URL:
+    DATABASES = {
+        'default': dj_database_url.parse(
+            DATABASE_URL,
+            conn_max_age=600,
+            ssl_require=True
+        )
+  }
     
 # else:
     
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+    
+#     # DATABASES = {
+#     #     'default': {
+#     #         'ENGINE': 'django.db.backends.sqlite3',
+#     #         'NAME': BASE_DIR / 'db.sqlite3',
+#     #     }
+#     # }
 
 
 
